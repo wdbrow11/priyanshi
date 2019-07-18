9d7da6478f5969c34290d5c12ba23601ec73a2bd
Will Brown <williambrown@williams-air.lan>
2019-07-18 00:01:10 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var shoutouts = [
+  "You are so nice, positive, and helpful.",
+"your positivity and that you recommended i joined this camp (tysm)",
+"you are great about pumping everyone up",
+"How easy you are to talk to. Even though I didn’t get to know you that well I still feel like I can talk to you.",
+"You are awesome!! I admire your confidence.",
+"How fun you are, especially during the games!",
+"You are so much help and I really appreciate that!",
+"your energy and how you are always ready to help.",
+"that you aren't afraid to be yourself",
+"How helpful you are to everybody!",
+"Your kindness and support and unwavering enthusiasm.",
+"how you talk to us and give us support!",
+"how funny you can be. You are really nice and I feel like I can joke around with you which makes class even more enjoyable",
+"stalker #1, you're very funny and I really enjoy talking with you, I really hope we still be friends after the camp.",
+"How motivating and encouraging you are. You always take time to make sure that everyone is comfortable and that is extremely admirable."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
